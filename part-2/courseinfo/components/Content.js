3ea540b5dc8bed13d1bc0d89e0b7490a49d93a6d
Ervin Rrahmani <ervinrrahmani12@gmail.com>
2021-08-25 20:11:06 +0200

    Update Content.js

--- part-2/courseinfo/components/Content.js
+++ part-2/courseinfo/components/Content.js
@@ -1,1 +1,24 @@
 
+import Content from "./Content";
+import Header from "./Header";
+import Total from "./Total";
+
+const Course = ({ courses }) => {
+
+
+  return (
+    <div>
+      {courses?.map((el, i) => {
+        return (
+          <div>
+            <Header text={el.name} />
+             <Content parts={el.parts} /> 
+            <Total parts={el.parts} /> 
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Course;
