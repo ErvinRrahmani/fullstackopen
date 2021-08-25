1718824ec6024740982c00162cb0b0031721a899
Ervin Rrahmani <ervinrrahmani12@gmail.com>
2021-08-25 20:14:23 +0200

    Update Total.js

--- part-2/courseinfo/components/Total.js
+++ part-2/courseinfo/components/Total.js
@@ -1,1 +1,10 @@
+const Total = ({ parts }) => {
+  const total = parts.reduce((s, p) => s + p.exercises, 0);
+  return (
+    <div>
+      <strong>total of {total} exercises</strong>
+    </div>
+  );
+};
 
+export default Total;
