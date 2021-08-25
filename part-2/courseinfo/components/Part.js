9a1f54e919b7c6bb4e827a878283b08f66eaac4c
Ervin Rrahmani <ervinrrahmani12@gmail.com>
2021-08-25 20:13:53 +0200

    part part

--- part-2/courseinfo/components/Part.js
+++ part-2/courseinfo/components/Part.js
@@ -1,1 +1,11 @@
+const Part = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((el, i) => {
+        return <li>{el?.name + " " + el?.exercises}</li>;
+      })}
+    </div>
+  );
+};
 
+export default Part;
