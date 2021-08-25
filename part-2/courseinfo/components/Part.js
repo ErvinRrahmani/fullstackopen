7c71e41c8e08a5db45baae57511b3e625f49d855
Ervin Rrahmani <ervinrrahmani12@gmail.com>
2021-08-25 20:17:23 +0200

    Update Part.js

--- part-2/courseinfo/components/Part.js
+++ part-2/courseinfo/components/Part.js
@@ -2,7 +2,11 @@ const Part = ({ parts }) => {
   return (
     <div>
       {parts.map((el, i) => {
-        return <li>{el?.name + " " + el?.exercises}</li>;
+        return (
+          <li>
+            {el?.name} {el?.exercises}
+          </li>
+        );
       })}
     </div>
   );
